b2c458ab85113f595f7f0a98378638be585e733d
BenSt099 <ben.steinhauer.de@gmail.com>
2024-11-21 18:37:24 +0100

    refactored AST and tests

--- code/src/egraph/AST.py
+++ code/src/egraph/AST.py
@@ -8,79 +8,80 @@ class ASTNode:
     right = ""
 
 
-representation = str()
+class AST:
 
+    def __init__(self, expression):
+        self.rootNode = self.process(expression)
 
-def to_string(node):
-    global representation
-    preorder(node)
-    representation = representation.strip()
-    return representation
+    representation = str()
+    rootNode = None
 
+    def to_string(self):
+        self.preorder(self.rootNode)
+        self.representation = self.representation.strip()
+        return self.representation
 
-def preorder(node):
-    global representation
-    if node.left == "" and node.right == "":
-        representation += str(node.key) + " "
-        return
-    else:
-        representation += str(node.key) + " "
-    preorder(node.left)
-    preorder(node.right)
+    def preorder(self, node):
+        if node.left == "" and node.right == "":
+            self.representation += str(node.key) + " "
+            return
+        else:
+            self.representation += str(node.key) + " "
+        self.preorder(node.left)
+        self.preorder(node.right)
 
-
-def process(expr):
-    root = ""
-    stack = deque()
-    for c in expr:
-        if c == "(":
-            if not stack:
-                k = ASTNode()
-                stack.append(k)
-                root = k
-            else:
+    def process(self, expr):
+        root = ""
+        stack = deque()
+        for c in expr:
+            if c == "(":
+                if not stack:
+                    k = ASTNode()
+                    stack.append(k)
+                    root = k
+                else:
+                    s = stack[-1]
+                    k = ASTNode()
+                    if s.left == "" and s.right == "":
+                        s.left = k
+                    elif s.left == "":
+                        s.left = k
+                    else:
+                        s.right = k
+                    stack.append(k)
+            elif c == ")":
+                stack.pop()
+            elif re.search("[0-9]+", c):
                 s = stack[-1]
-                k = ASTNode()
                 if s.left == "" and s.right == "":
+                    k = ASTNode()
+                    k.key = c
                     s.left = k
                 elif s.left == "":
+                    k = ASTNode()
+                    k.key = c
                     s.left = k
                 else:
+                    k = ASTNode()
+                    k.key = c
                     s.right = k
-                stack.append(k)
-        elif c == ")":
-            stack.pop()
-        elif re.search("[0-9]+", c):
-            s = stack[-1]
-            if s.left == "" and s.right == "":
-                k = ASTNode()
-                k.key = c
-                s.left = k
-            elif s.left == "":
-                k = ASTNode()
-                k.key = c
-                s.left = k
+            elif c == "/" or c == "*" or c == "+" or c == "-" or c == "<<" or c == ">>":
+                s = stack[-1]
+                s.key = c
+            elif c == " ":
+                pass
             else:
-                k = ASTNode()
-                k.key = c
-                s.right = k
-        elif c == "/" or c == "*" or c == "+" or c == "-" or c == "<<" or c == ">>":
-            s = stack[-1]
-            s.key = c
-        elif c == " ":
-            pass
-        else:
-            s = stack[-1]
-            if s.left == "" and s.right == "":
-                k = ASTNode()
-                k.key = c
-                s.left = k
-            elif s.left == "":
-                k = ASTNode()
-                k.key = c
-                s.left = k
-            else:
-                k = ASTNode()
-                k.key = c
-                s.right = k
-    return root
+                s = stack[-1]
+                if s.left == "" and s.right == "":
+                    k = ASTNode()
+                    k.key = c
+                    s.left = k
+                elif s.left == "":
+                    k = ASTNode()
+                    k.key = c
+                    s.left = k
+                else:
+                    k = ASTNode()
+                    k.key = c
+                    s.right = k
+        return root
--- code/tests/test_ast.py
+++ code/tests/test_ast.py
@@ -1,11 +1,9 @@
 from AST import *
 
 def test_ast():
-    ast = process("(/ (* a 2) 2)")
-    ast_str = to_string(ast)
-    assert ast_str == "/ * a 2 2"
+    ast = AST("(/ (* a 2) 2)")
+    assert "/ * a 2 2" == ast.to_string()
 
 def test_ast_2():
-    ast2 = process("(+ a (* a 2) )")
-    ast_str2 = to_string(ast2)
-    assert ast_str2 == "+ a * a 2"
+    ast2 = AST("(+ a (* a 2) )")
+    assert "+ a * a 2" == ast2.to_string()
