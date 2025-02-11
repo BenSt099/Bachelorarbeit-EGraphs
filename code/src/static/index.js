d9efa0adf2c80a70f52f817db04f668a7af1670d
BenSt099 <ben.steinhauer.de@protonmail.com>
2025-02-11 16:45:31 +0100

    updated thesis, fixed js file

--- code/src/static/index.js
+++ code/src/static/index.js
@@ -496,7 +496,7 @@ function uploadSession() {
                         addMessageToStatusBar("[WARN]", value['msg']);
                     } else {
                         addMessageToStatusBar("[INFO]", value['msg']);
-                        if (value["payload"] !== []) {
+                        if (String(value["payload"])) {
                             addMessageToStatusBar("[INFO]", "Applied rules in last session: " + value['payload']);
                             loadData();
                         }
